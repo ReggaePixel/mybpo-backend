d9447daeca09902e2033a2133973ba5b29cb130b
ReggaePixel® <reggaepixel876@gmail.com>
2026-04-10 13:52:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -105,7 +105,10 @@ const fs = require("fs");
 const upload = multer({ dest: "uploads/" });
 
 const storage = new Storage({
-  keyFilename: "mybpo-platform-dc4d69bdbe41.json"
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  },
 });
 
 const bucketName = "mybpo-platform-storage";
